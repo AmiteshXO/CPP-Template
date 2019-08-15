5eaab7b90a53979c7a9e1ea12ab5f662871474b5
Amitesh Acharya <30487840+AmiteshXO@users.noreply.github.com>
2019-08-15 15:24:03 +0530

    Add files via upload

---
+++ temp.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mod 1e9+7
+#define FOR(a,c)   for ( int (a)=0; (a)<(c); (a)++)
+#define FORL(a,b,c)  for ( int (a)=(b); (a)<=(c); (a)++)
+#define FORR(a,b,c)  for (  int (a)=(b); (a)>=(c); (a)--)
+typedef long long int ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+#define MP make_pair
+
+// ll gcd(ll a, ll b) 
+// { 
+//     if (a == 0) 
+//         return b; 
+//     return gcd(b % a, a); 
+// } 
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL); 
+    
+    return 0;
+}
